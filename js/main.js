2f502e581ad8587dfdc4f1be9330f1ce37c4a79f
FedericoBellezza <federico.bellezza2001@gmail.com>
2024-11-11 14:58:24 +0100

    add calcolaEta() method

--- js/main.js
+++ js/main.js
@@ -1,19 +1,26 @@
+const date = new Date();
+const year = date.getFullYear();
+
 class Veicolo {
 	brand;
-	year;
+	constructionYear;
 	color;
 
-	constructor(brand, year, color) {
+	constructor(brand, constructionYear, color) {
 		this.brand = brand;
-		this.year = year;
+		this.constructionYear = constructionYear;
 		this.color = color;
 	}
 
 	informazioni() {
-		return `La marca della macchina è: ${this.brand}, l'anno di costruzione è ${this.year} ed è di colore ${this.color}`;
+		return `La marca della macchina è: ${this.brand}, l'anno di costruzione è ${this.constructionYear} ed è di colore ${this.color}.`;
+	}
+	calcolaEta() {
+		return year - this.constructionYear;
 	}
 }
 
 const myCar = new Veicolo('Fiat', 2019, 'blu');
 
 console.log(myCar.informazioni());
+console.log(`La macchina ha ${myCar.calcolaEta()} anni.`);
